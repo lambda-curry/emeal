057b4cd16862bea72b86be257beaacda7618ba0e
Derek Wene <derek.wene@gmail.com>
2020-04-03 00:35:58 -0500

    Add ability to download csv of all emails for project, also started working on some analytics

--- api/deploy.sh
+++ api/deploy.sh
@@ -4,3 +4,4 @@ yarn install
 yarn build
 docker build . -t djrobotfreak/emeal-backend:$DOCKERTAG
 docker push djrobotfreak/emeal-backend:$DOCKERTAG
+kubectl -n emeal-prod set image deployment/server server=djrobotfreak/emeal-backend:$DOCKERTAG
--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,4 +1,6 @@
+DOCKERTAG=`date +"%s"`
 yarn install
 yarn build
-docker build . -t djrobotfreak/emeal-frontend
-docker push djrobotfreak/emeal-frontend
+docker build . -t djrobotfreak/emeal-frontend:$DOCKERTAG
+docker push djrobotfreak/emeal-frontend:$DOCKERTAG
+kubectl -n emeal-prod set image deployment/frontend frontend=djrobotfreak/emeal-frontend:$DOCKERTAG
