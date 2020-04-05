06a09f873207b2c9f616e07ca01956229c3589c5
Derek Wene <derek.wene@gmail.com>
2020-04-04 19:36:34 -0500

    allow dev.emeal.me with cors

--- api/src/middleware/cors.ts
+++ api/src/middleware/cors.ts
@@ -1,14 +1,19 @@
 import cors from 'cors';
-const corsWhitelist = ['app.emeal.me', 'local.emeal.me', 'localhost'];
+const corsWhitelist = [
+  'app.emeal.me',
+  'local.emeal.me',
+  'dev.emeal.me',
+  'localhost',
+];
 const corsOptions = {
   credentials: true,
-  origin: function(origin: string, callback: any) {
-    if (!origin || corsWhitelist.some(host => origin.indexOf(host) > -1)) {
+  origin: function (origin: string, callback: any) {
+    if (!origin || corsWhitelist.some((host) => origin.indexOf(host) > -1)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
 };
 
 export const corsMiddleware = cors(corsOptions);
