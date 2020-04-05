fd5002902a28e5b00c02d66cabd6f2d06d1167ef
Derek Wene <derek.wene@gmail.com>
2020-04-04 20:38:42 -0500

    temporarily disabling cors

--- api/src/services/stripe.ts
+++ api/src/services/stripe.ts
@@ -1,6 +1,15 @@
 import Stripe from 'stripe';
+import { UserDocument } from '../models/User';
 
 const stripeClient = new Stripe('', {
   protocol: 'https',
   apiVersion: '2020-03-02',
 });
+
+export async function createStripeCustomer(user: UserDocument) {
+  const customer = await stripeClient.customers.create({
+    name: user.name,
+    email: user.email,
+    metadata: { id: user.id },
+  });
+}
