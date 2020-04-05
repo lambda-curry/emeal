74854b6fb256db5a1b3b69d7ca4453cacf589851
Jake Ruesink <jaruesink@gmail.com>
2020-04-05 13:36:38 -0500

    embed page now has embed code

--- frontend/src/pages/EmbedPage.tsx
+++ frontend/src/pages/EmbedPage.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-// import './embed-page.scss';
+import { useSession } from '../state/session/SessionProvider';
+import { selectCurrentProject } from '../state/session/SessionSelectors';
+import './embed-page.scss';
 
-export const EmbedPage = () => (
-  <div className='page embed'>
-    <div className='page-container'>
-      <h2>Embed Page</h2>
+export const EmbedPage = () => {
+  const { state } = useSession();
+  const currentProject = selectCurrentProject(state);
+
+  return (
+    <div className='page embed'>
+      <div className='page-container'>
+        <div className='page-item'>
+          <h3>Embed Code</h3>
+          <code>
+            {`<script type="text/javascript">window.emealCouponId = '${currentProject.id}';</script>`}
+            <br />
+            {`<script async src="https://app.emeal.me/modal/dist/emeal-embed.min.js"></script>`}
+          </code>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
