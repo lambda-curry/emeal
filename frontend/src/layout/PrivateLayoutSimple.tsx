a2d033e2bd3634ee644f6bb94b27886272dc6ebf
Jake Ruesink <jaruesink@gmail.com>
2020-04-12 16:38:38 -0500

    eslint ignore for anchor tag link

--- frontend/src/layout/PrivateLayoutSimple.tsx
+++ frontend/src/layout/PrivateLayoutSimple.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { PrivateRoutes } from '../routes/PrivateRoutes';
 import { LogoType } from '../components/brand/LogoType';
