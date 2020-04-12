04ed845f42a5f2234d4f5bbd3c5d0e7ff2558125
Jake Ruesink <jaruesink@gmail.com>
2020-04-12 16:32:57 -0500

    adding back to sign in link on private layout simple

--- frontend/src/layout/PrivateLayoutSimple.tsx
+++ frontend/src/layout/PrivateLayoutSimple.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { PrivateRoutes } from '../routes/PrivateRoutes';
 import { LogoType } from '../components/brand/LogoType';
+import { useSession } from '../state/session/SessionProvider';
 
-export const PrivateLayoutSimple = () => (
-  <div className='private-layout-simple'>
-    <LogoType />
-    <PrivateRoutes />
-  </div>
-);
+export const PrivateLayoutSimple = () => {
+  const { actions } = useSession();
+
+  return (
+    <div className='private-layout-simple'>
+      <div className='private-layout-simple-header'>
+        <a
+          className='private-layout-simple-logout'
+          href='javascript:void(0);'
+          onClick={actions.destroySession}
+        >
+          Back to Sign In
+        </a>
+        <LogoType />
+      </div>
+      <PrivateRoutes />
+    </div>
+  );
+};
