89efbe44f1f4fd5d941e9d974b4d40bd828b7b58
Derek Wene <derek.wene@gmail.com>
2020-04-13 16:13:12 -0500

    add names to from emails

--- api/src/services/mail.ts
+++ api/src/services/mail.ts
@@ -13,7 +13,7 @@ export const sendForgotPasswordEmail = async (
 ) => {
   const message = {
     templateId: 'd-3af04204d9974d66b6b09420a4b69e4f',
-    from: SYSTEM_EMAIL_FROM,
+    from: { name: 'Emeal Support', email: SYSTEM_EMAIL_FROM },
     to: user.email,
     dynamicTemplateData: {
       resetPasswordUrl: `https://app.emeal.me/change-password/${token}`,
@@ -25,7 +25,7 @@ export const sendForgotPasswordEmail = async (
 export const sendCouponEmail = async (coupon: CouponDocument) => {
   const message = {
     templateId: 'd-ee9d35566b2f461dace56636ed0785c8',
-    from: SYSTEM_EMAIL_FROM,
+    from: { name: coupon.projectName, email: SYSTEM_EMAIL_FROM },
     to: coupon.email,
     dynamicTemplateData: {
       projectName: coupon.projectName,
