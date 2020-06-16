40ca593143a558d7f4502c45489ec016ab296d43
Derek Wene <derek.wene@gmail.com>
2020-06-15 22:18:56 -0500

    add healthcheck that depends on the database

--- api/src/controllers/routes.ts
+++ api/src/controllers/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { router as userRouter } from './user';
 import { router as projectRouter } from './project';
 import { router as authRouter } from './auth';
@@ -7,11 +7,18 @@ import { router as analyticsRouter } from './analytics';
 import { router as sessionRouter } from './session';
 import { router as paymentRouter } from './payment';
 import { router as stripeWebhookRouter } from './stripe';
+import mongoose from 'mongoose';
 
 export const apiRouter = Router();
 
 apiRouter
   .get('/ping', (req, res) => res.json({ message: 'pong' }))
+  .get('/healthcheck', (req: Request, res: Response) => {
+    if (mongoose.connection.readyState === 1) {
+      return res.json({ message: 'healthy' });
+    }
+    return res.status(500).json({ message: 'mongoose connection unhealthy' });
+  })
   .get('/error', (req, res) => {
     throw new Error('Boom! This is broken');
   })
